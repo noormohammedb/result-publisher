0d1f3d45b5445a37228009227f133ad225ac7e8c
Noor Mohammed B <nooru.mohad@gmail.com>
2020-12-08 18:41:39 +0530

    TEST: add student
    testing with jwt and without jwt
    ignored test.env

--- tests/main.test.js
+++ tests/main.test.js
@@ -17,10 +17,21 @@ const server = require("../app/app");
 const teacherLoginPayload = {
    email: "tester@mocha.com",
    password: "password"
-}
+},
+   studentPayload = {
+      name: "tester",
+      registerNumber: 77,
+      subject1: 4,
+      subject2: 1,
+      subject3: 6
+   }
+
+let authToken = "";
 
 const teacherSignup = "/v1/signup",
-   teacherlogn = "/v1/login";
+   teacherlogin = "/v1/login",
+   addStudent = "/v1/add_student",
+   showResult = "/v1";
 
 chai.use(chaiHttp);
 
@@ -58,16 +69,82 @@ describe("==>> Testing result publication system", () => {
    });
 
    describe("==>> login teacher", () => {
-      it(`requesting ${teacherSignup}`, (done) => {
+      it(`requesting ${teacherlogin}`, (done) => {
          chai.request(server)
-            .post(teacherSignup)
+            .post(teacherlogin)
             .send(teacherLoginPayload)
             .end((err, res) => {
                // if (err) throw err;
+               res.status.should.eql(200);
+               res.body.should.property("status", true);
+               console.log(res.body.data.token);
+               authToken = res.body.data.token;
+               done();
+            });
+      });
+   });
+
+   describe("==>> add student without login teacher", () => {
+      it(`requesting ${addStudent}`, (done) => {
+         chai.request(server)
+            .post(addStudent)
+            .send(studentPayload)
+            .end((err, res) => {
+               // if (err) throw err;
+               console.log(res.body);
+               res.status.should.eql(401);
+               res.body.should.property("status", false);
+               done();
+            });
+      });
+   });
+
+   describe("==>> add student with logged in teacher", () => {
+      it(`requesting ${addStudent}`, (done) => {
+         chai.request(server)
+            .post(addStudent)
+            .send(studentPayload)
+            .set("Authorization", `bearar ${authToken}`)
+            .end((err, res) => {
+               // if (err) throw err;
+               console.log(res.body);
                res.status.should.eql(200);
                res.body.should.property("status", true);
                done();
             });
       });
    });
+
+   describe("==>> check student result", () => {
+      it(`requesting ${showResult}`, (done) => {
+         chai.request(server)
+            .get(showResult)
+            .send({ registerNumber: studentPayload.registerNumber })
+            .set("Authorization", `bearar ${authToken}`)
+            .end((err, res) => {
+               // if (err) throw err;
+               res.status.should.eql(200);
+               res.body.should.property("status", true);
+               res.body.data.should.property("registerNumber", studentPayload.registerNumber);
+               done();
+            });
+      });
+   });
+
+   describe("==>> check student result", () => {
+      it(`requesting ${showResult}`, (done) => {
+         chai.request(server)
+            .get(showResult)
+            .send({ registerNumber: studentPayload.registerNumber })
+            .set("Authorization", `bearar ${authToken}`)
+            .end((err, res) => {
+               // if (err) throw err;
+               res.status.should.eql(200);
+               res.body.should.property("status", true);
+               res.body.data.should.property("registerNumber", studentPayload.registerNumber);
+               done();
+            });
+      });
+   });
+
 });
