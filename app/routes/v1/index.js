472268b5e03c52f2fb6f392034d426eaa3e1ead0
Noor Mohammed B <nooru.mohad@gmail.com>
2020-12-08 15:50:36 +0530

    ADD: edit student
    edit student's record

--- app/routes/v1/index.js
+++ app/routes/v1/index.js
@@ -5,6 +5,7 @@ app.use("/signup", require("./teacherSignupRouter"));
 app.use("/login", require("./teacherLoginRoutet"));
 app.use("/add_student", require("./addStudentRouter"));
 app.use("/delete_student", require("./deleteStudentRouter"));
+app.use("/edit_student", require("./editStudentRouter"));
 
 app.all("*", (req, res) => {
    res.status(400).json({ message: "Not Found v1" });
---
+++ app/services/editStudentLogic.js
@@ -0,0 +1,43 @@
+const StudentModel = require("../models/studentModel")
+const findStudentRegNum = require("../utils/findStudentRegNum")
+module.exports = async (payload) => {
+   const dbResFind = await findStudentRegNum(payload);
+   if (dbResFind) {
+      console.log(dbResFind);
+      try {
+         const dbRes = await StudentModel.updateOne(
+            { registerNumber: payload.registerNumber },
+            {
+               $set: {
+                  ...payload
+               }
+            }
+         );
+         /* name: payload.name,
+         subject1: payload.subject1,
+         subject2: payload.subject2,
+         subject3: payload.subject3, */
+         return {
+            statusCode: 200,
+            json: {
+               status: true,
+               messae: "student setails edited successfully",
+               data: {},
+            }
+         };
+      } catch (error) {
+         console.log("error in edit student db operation");
+         console.error(error);
+      }
+   } else {
+      console.log("student not exist for exit");
+      return {
+         statusCode: 400,
+         json: {
+            status: false,
+            messae: "student not exist",
+            data: {},
+         }
+      };
+   }
+}
