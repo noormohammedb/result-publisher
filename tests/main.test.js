9d7ed95ebd0833222072e0294c8d31f4d3bce879
Noor Mohammed B <nooru.mohad@gmail.com>
2020-12-08 18:56:30 +0530

    TEST: edit student data
    edit and check student data is edited

--- tests/main.test.js
+++ tests/main.test.js
@@ -17,21 +17,27 @@ const server = require("../app/app");
 const teacherLoginPayload = {
    email: "tester@mocha.com",
    password: "password"
-},
-   studentPayload = {
-      name: "tester",
-      registerNumber: 77,
-      subject1: 4,
-      subject2: 1,
-      subject3: 6
-   }
+}, studentPayload = {
+   name: "tester",
+   registerNumber: 77,
+   subject1: 4,
+   subject2: 1,
+   subject3: 6
+}, editStudentPayload = {
+   name: "tester edited",
+   registerNumber: 77,
+   subject1: 5,
+   subject2: 5,
+   subject3: 5
+}
 
-let authToken = "";
+let authToken = "", totalMark = 0;
 
 const teacherSignup = "/v1/signup",
    teacherlogin = "/v1/login",
    addStudent = "/v1/add_student",
-   showResult = "/v1";
+   showResult = "/v1",
+   editStudent = "/v1/edit_student";
 
 chai.use(chaiHttp);
 
@@ -120,28 +126,61 @@ describe("==>> Testing result publication system", () => {
          chai.request(server)
             .get(showResult)
             .send({ registerNumber: studentPayload.registerNumber })
-            .set("Authorization", `bearar ${authToken}`)
             .end((err, res) => {
                // if (err) throw err;
                res.status.should.eql(200);
                res.body.should.property("status", true);
                res.body.data.should.property("registerNumber", studentPayload.registerNumber);
+               totalMark = res.body.data.total;
+               console.log(`==============================${totalMark}==========================`);
                done();
             });
       });
    });
 
-   describe("==>> check student result", () => {
+   describe("==>> edit student details without logged in teacher", () => {
+      it(`requesting ${editStudent}`, (done) => {
+         chai.request(server)
+            .patch(editStudent)
+            .send(editStudentPayload)
+            .end((err, res) => {
+               // if (err) throw err;
+               console.log(res.body);
+               res.status.should.eql(401);
+               res.body.should.property("status", false);
+               done();
+            });
+      });
+   });
+
+   describe("==>> edit student details with logged in teacher", () => {
+      it(`requesting ${editStudent}`, (done) => {
+         chai.request(server)
+            .patch(editStudent)
+            .set("Authorization", `bearar ${authToken}`)
+            .send(editStudentPayload)
+            .end((err, res) => {
+               // if (err) throw err;
+               console.log(res.body);
+               res.status.should.eql(200);
+               res.body.should.property("status", true);
+               done();
+            });
+      });
+   });
+
+   describe("==>> check student result is edited", () => {
       it(`requesting ${showResult}`, (done) => {
          chai.request(server)
             .get(showResult)
             .send({ registerNumber: studentPayload.registerNumber })
-            .set("Authorization", `bearar ${authToken}`)
             .end((err, res) => {
                // if (err) throw err;
                res.status.should.eql(200);
                res.body.should.property("status", true);
                res.body.data.should.property("registerNumber", studentPayload.registerNumber);
+               res.body.data.total.should.not.equal(totalMark);
+               console.log(`==============================${res.body.data.total}==========================`);
                done();
             });
       });
