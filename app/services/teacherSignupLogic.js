647751b53a38fe7d5b3f82303fa6f206e14b0757
Noor Mohammed B <nooru.mohad@gmail.com>
2020-12-08 13:34:12 +0530

    ADD: jwt and bcrupt
    signing password with bcrypto
    signing jwt with _id and name

--- app/services/teacherSignupLogic.js
+++ app/services/teacherSignupLogic.js
@@ -1,5 +1,10 @@
+const bcrypt = require("bcrypt")
+const { signJwt } = require("../utils/createJwt")
 const TeacherModel = require("../models/teacherModel")
 module.exports = async (payload) => {
+
+   payload.password = await bcrypt.hash(payload.password, 10)
+
    const newTeacher = new TeacherModel({
       name: payload.name,
       emial: payload.emial,
@@ -8,7 +13,8 @@ module.exports = async (payload) => {
    });
    try {
       const dbRes = await newTeacher.save();
-      return dbRes;
+      const token = signJwt(dbRes)
+      return token;
    } catch (error) {
       console.log("error in teacher signup db operation");
       console.error(error);
---
+++ app/utils/createJwt.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+function signJwt(payload) {
+   try {
+      const { _id, name } = payload;
+      const token = jwt.sign({ _id, name }, process.env.JWT_SECRET, { expiresIn: "15m" });
+      return token;
+   } catch (error) {
+      console.log("error in jwt sign");
+      console.error(error);
+   }
+}
+
+
+module.exports = { signJwt };
